8fc7cb58a639f294d2b486b8df21132bf57c5169
codepilot777 <codepilot777@gmail.com>
2023-06-11 20:46:12 +0800

    complete navbar ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className="text-rose-500 text-2xl">
         Hello World! Airbnb
       </div>
     </main>
